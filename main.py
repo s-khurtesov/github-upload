349339164d4c66e315df19880197f2059e73600b
Stanislav Khurtesov <noreply@github.com>
2020-12-05 21:55:55 +0300

    Add printing some png data

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ with open(r'./data.txt', 'w') as f:
 with open(r'./data.txt') as f:
     print('Data:', f.read())
 
+with open(r'./cell.png', 'rb') as f:
+    png = f.read()
+
+print(type(png), len(png), '\nSet:', set(png))
+
 print('Nice project though!')
 
